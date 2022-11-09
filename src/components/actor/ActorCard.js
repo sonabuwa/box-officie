44dc4499ee9799cd81635c7edf8c0d3812c2e61a
sonali <sonalibuwade7020@gmail.com>
2022-11-10 02:33:24 +0530

    installed styled componets and created styles for search cards

--- src/components/actor/ActorCard.js
+++ src/components/actor/ActorCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+// import { SearchCard } from '../styled';
+import { StyledActorCard } from './ActorCard.styled';
 
 const ActorCard = ({ image, name, gender, country, birthday, deathday }) => {
   return (
-    <div>
-      <div>
+    <StyledActorCard>
+      <div className="img-wrapper">
         <img src={image} alt="actor" />
       </div>
       <h1>
@@ -12,7 +14,7 @@ const ActorCard = ({ image, name, gender, country, birthday, deathday }) => {
       <p>{country ? `Comes from ${country}` : 'No country known'}</p>
       {birthday ? <p>Born {birthday}</p> : null}
       <p>{deathday ? `Died ${deathday}` : 'Alive'}</p>
-    </div>
+    </StyledActorCard>
   );
 };
 
